cafc9b7af1f8d159e3e4b4eb0d5276a4370e599a
Crew Spence <csspence123@gmail.com>
2020-01-26 05:54:16 -0600

    Wrote working solution for supersize_me.

--- supersize_me.js
+++ supersize_me.js
@@ -7,5 +7,19 @@ superSize(12) // 21
 If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
 */
 
-function superSize(num){
+const superSize = (num) => {
+  const numStr = num.toString();
+  let numArr = [];
+  let newStr = '';
+
+  for(let i = 0; i < numStr.length; i++) {
+    numArr.push(numStr[i]);
+  }
+  numArr = numArr.sort(function(a, b){return b-a});
+  
+  for(let j = 0; j < numArr.length; j++) {
+    newStr += numArr[j];
+  }
+
+  return (parseInt(newStr));
 }
